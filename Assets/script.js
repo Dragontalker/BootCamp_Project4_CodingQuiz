1b55b2e48365f47e4a719e4f34ea07df4fe578a7
Richard Yang <richard.yang.tong@gmail.com>
2021-01-24 12:23:41 -0500

    Change the datatype of quizQuestions to array for better accessibililty, also change the structue of question 1, then tested all cases.

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,25 @@
-const quizQuestions = {
-    firstQuestion: {
+const quizQuestions = [
+    firstQuestion = {
         question: "Commoly used data types DO NOT include: ",
         answer: "3. alerts",
+        firstOption: "1. strings",
+        secondOption: "2. booleans",
+        thirdOption: "3. alerts",
+        fourthOption: "4. numbers",
         get chooseFirst () {
-            return this.answer === "1. strings";
+            return this.answer === this.firstOption;
         },
         get chooseSecond () {
-            return this.answer === "2. booleans";
+            return this.answer === this.secondOption;
         },
         get chooseThird () {
-            return this.answer === "3. alerts";
+            return this.answer === this.thirdOption;
         },
         get chooseFourth () {
-            return this.answer === "4. numbers";
+            return this.answer === this.fourthOption;
         }
     },
-    secondQuestion: {
+    secondQuestion = {
         question: "The condition in an if / else statement is enchosed within ________.",
         answer: "3. parentheses",
         get chooseFirst () {
@@ -31,7 +35,7 @@ const quizQuestions = {
             return this.answer === "4. square brackets";
         }
     },
-    thirdQuestion: {
+    thirdQuestion = {
         question: "Arrays in JavaScript can be used to store ________.",
         answer: "4. all of the above",
         get chooseFirst () {
@@ -47,7 +51,7 @@ const quizQuestions = {
             return this.answer === "4. all of the above";
         }
     },
-    fourthQuestion: {
+    fourthQuestion = {
         question: "String values must be enclosed within ________ when being assigned to variables.",
         answer: "2. curly brackets",
         get chooseFirst () {
@@ -63,7 +67,7 @@ const quizQuestions = {
             return this.answer === "4. parentheses";
         }
     },
-    fifthQuestion: {
+    fifthQuestion = {
         question: "A very useful tool used during development and debugging for print content to the debugger is:",
         answer: "4. console.log",
         get chooseFirst () {
@@ -79,11 +83,26 @@ const quizQuestions = {
             return this.answer === "4. console.log";
         }
     }
-}
+]
+
+// Test Question 1:
+console.log(quizQuestions[0].chooseFirst);
+console.log(quizQuestions[0].chooseSecond);
+console.log(quizQuestions[0].chooseThird);
+console.log(quizQuestions[0].chooseFourth);
 
 
-// Test: Question 5.
-console.log(quizQuestions.fifthQuestion.chooseFirst);
-console.log(quizQuestions.fifthQuestion.chooseSecond);
-console.log(quizQuestions.fifthQuestion.chooseThird);
-console.log(quizQuestions.fifthQuestion.chooseFourth);
+// counter = 0;
+
+// const printQuestion = (index) => {
+//     let currentQuestion = quizQuestions[index];
+//     let main = document.querySelector("main");
+    
+//     let newQuestion = document.createElement("h2");
+//     newQuestion.innerText = currentQuestion.question;
+
+//     let newAnswers = document.createElement("ul");
+    
+//     let newFirstOption = document.createElement("li")
+//     newFirstOption.innerText = currentQuestion.
+// }
