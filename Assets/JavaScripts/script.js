a4ec06e24ddbc7635af9441ee84a530d3d685113
Richard Yang <richard.yang.tong@gmail.com>
2021-01-26 10:05:56 -0500

    Updated the looks of scoreboard.

--- Assets/JavaScripts/script.js
+++ Assets/JavaScripts/script.js
@@ -208,7 +208,7 @@ const viewHighScore = () => {
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         const value = localStorage.getItem(key);
-        main.children[0].innerHTML += `<h2> Log #${i+1}: ${key} - ${value} </h2> <br/>`;
+        main.children[0].innerHTML += `<h2> ${i+1}. ${key} - ${value} </h2> <br/>`;
     }
 
     let homeBtn = document.createElement("button");
